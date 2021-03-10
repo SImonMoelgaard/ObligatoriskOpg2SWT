8b8d2bb17b538bf7184fc77b7496c48c9631439c
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2021-03-10 09:03:49 +0100

    Tilføjet de givne klasser

--- Consoleapplication/Program.cs
+++ Consoleapplication/Program.cs
@@ -1,21 +1,47 @@
-﻿using System;
+﻿    using System;
 
-namespace Consoleapplication
-{
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fuck you");
-            Console.WriteLine("NAJ");
+				// Assemble your system here from all the classes
 
+            bool finish = false;
+            do
+            {
+                string input;
+                System.Console.WriteLine("Indtast E, O, C, R: ");
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) continue;
 
+                switch (input[0])
+                {
+                    case 'E':
+                        finish = true;
+                        break;
 
-        }
+                    case 'O':
+                        door.OnDoorOpen();
+                        break;
 
-        public void test()
-        {
+                    case 'C':
+                        door.OnDoorClose();
+                        break;
 
+                    case 'R':
+                        System.Console.WriteLine("Indtast RFID id: ");
+                        string idString = System.Console.ReadLine();
+
+                        int id = Convert.ToInt32(idString);
+                        rfidReader.OnRfidRead(id);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (!finish);
         }
     }
-}
+
+
