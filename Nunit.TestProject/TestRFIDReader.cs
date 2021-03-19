293cb71b0a9648dc531ccf972356318e59c67158
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2021-03-19 16:37:07 +0100

    TEst

--- Nunit.TestProject/TestRFIDReader.cs
+++ Nunit.TestProject/TestRFIDReader.cs
@@ -8,38 +8,41 @@ namespace Nunit.TestProject
     public class Tests
     {
         private IRFIDReader _uut;
+        private RFIDEventArgs RFIDEvent;
         [SetUp]
         public void Setup()
-        {
-            
+        { 
             _uut = new RfidReader();
+            RFIDEvent = null;
+            _uut.RfidChangedEvent += (sender, args) => { RFIDEvent = args;};
+
         }
 
         [Test]
-        public void CtorRFID()
+        public void RFIDSet()
         {
             _uut.CardID = 10;
-            Assert.That(_uut.CardID == 10);
+            Assert.That(_uut.CardID, Is.EqualTo(10));
 
 
         }
 
         [Test]
-        public void RFIDEventHigher()
+        public void NewRFIDSet()
         {
-            double lastRFID = 88888888;
-            _uut.RfidChangedEvent += (sender, args) => lastRFID = args.Id;
+            _uut.CardID = 10;
+            
 
-            Assert.That(lastRFID, Is.EqualTo(88888888));
+            Assert.That(RFIDEvent, Is.EqualTo(10));
         }
 
         [Test]
         public void RFIDEventLower()
         {
            // int lastRFID = 0;
-            
-            
-           // _uut.RfidChangedEvent += (sender, args) => args.Id=lastRFID;
+
+
+           _uut.RfidChangedEvent += (sender, args) => args.Id;
            Assert.That(_uut.CardID, Is.Zero);
         }
     }
