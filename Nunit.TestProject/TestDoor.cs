37aee38979b0ccbe90a21b54c0ccebc71832ca42
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2021-03-23 11:01:58 +0100

    d

--- Nunit.TestProject/TestDoor.cs
+++ Nunit.TestProject/TestDoor.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ClassLibrary;
+using ClassLibrary.DoorObserver;
 using NUnit.Framework;
 
 namespace Nunit.TestProject
 {
     public class TestDoor
     {
+        private IDoor _uut;
         [SetUp]
         public void Setup()
         { 
-            
+            _uut = new Door();
 
         }
 
+
+        //EKsempel på dørtest
         [Test]
-        public void METHOD()
+        public void CloseDoor()
         {
-            
+            _uut.isDoorClosed = false;
+            _uut.CloseDoor();
+            Assert.That(_uut.isDoorClosed=true);
         }
 
 
