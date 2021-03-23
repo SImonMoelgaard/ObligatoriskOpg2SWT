75a564976fbf1e4081aebe918fa8b9243faa5986
Vinni Binderup <201904697@post.au.dk>
2021-03-23 15:45:22 +0100

    Test Logging og Display

--- Nunit.TestProject/TestDisplay.cs
+++ Nunit.TestProject/TestDisplay.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ClassLibrary;
+using ClassLibrary.Display;
+using NUnit.Framework;
 
 namespace Nunit.TestProject
 {
     public class TestDisplay
     {
+        private IDisplay _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Display();
+        }
+
+        [Test]
+        public void PrintMessage()
+        {
+            string testString = "Message";
+            _uut.PrintMessage(testString);
+
+        }
     }
 }
--- Nunit.TestProject/TestLogging.cs
+++ Nunit.TestProject/TestLogging.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ClassLibrary.Logging;
 using NUnit.Framework;
 
 namespace Nunit.TestProject
 {
     public class TestLogging
     {
+        private ILogging _uut;
         [SetUp]
         public void Setup()
         { 
-            
-
+            _uut = new Logging();
         }
 
         [Test]
-        public void METHOD()
+        public void Log()
         {
             
         }
