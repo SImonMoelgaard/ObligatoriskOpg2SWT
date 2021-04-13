96c8fc792d42a38517c6e342cc0ee0572c90a8bc
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2021-04-13 11:08:49 +0200

    s

--- Nunit.TestProject/TestStationControl.cs
+++ Nunit.TestProject/TestStationControl.cs
@@ -245,6 +245,8 @@ namespace Nunit.TestProject
             _RFID.RfidChangedEvent += Raise.EventWith(this, new RFIDEventArgs(){Id = 5});
             Assert.That(_uut.oldID, Is.EqualTo(5));
         }
+       
+        [Test]
         public void TEstOldIDSet()
         {
             _uut.oldID = 5;
